9d6a6cf29be0157fe267f0dddeef520f81b711ec
Mega-Roman <rshiltsov@gmail.com>
2024-09-07 13:16:54 +1200

    added input validation for quit

--- game.py
+++ game.py
@@ -4,6 +4,10 @@ print("If you wish to quit type 'quit' into the console")
 while True:
     player_choice = input("Please enter your choice from rock, paper or scissors. ")
 
+    if player_choice.lower() == "quit" or player_choice.lower() == "q":
+      print("Thank you so much for playing our game")
+      break
+
     if player_choice.lower() == "rock" or player_choice.lower() == "r":
         print('Chosen rock')
 
@@ -14,11 +18,8 @@ while True:
         print('Chosen scissors')
 
     else:
-        print('please choose a valid input')        
-
-    if player_choice.lower() == "quit" or player_choice.lower() == "q":
-      print("Thank you so much for playing our game")
-      break
+        print('please choose a valid input')  
+        continue      
 
     computer_choice = random.randint(1,3)
     if computer_choice == 1:
