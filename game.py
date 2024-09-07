d0532aea281f20ce6a283e46b85d31c4b7c6d64f
RhysDaBoi <rhysww1@outlook.com>
2024-09-07 12:33:14 +1200

    I have added message to quit and a quit function

--- game.py
+++ game.py
@@ -1,8 +1,12 @@
 import random
 print("Welcome to our rock paper scissors game!")
+print("If you wish to quit type 'quit' into the console")
 while True:
     player_choice = input("Please enter your choice from rock, paper or scissors. ")
+    if player_choice.lower() == "quit":
+        break
     print(player_choice)
+
     computer_choice = random.randint(1,3)
     if computer_choice == 1:
         print("You chose " + player_choice + ", I chose rock")
@@ -10,3 +14,4 @@ while True:
         print("You chose " + player_choice + ", I chose paper")
     if computer_choice == 3:
         print("You chose " + player_choice + ", I chose scissors")
+
