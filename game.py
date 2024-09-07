79976932cea02180f31a46ae2802d19719befa53
Gabriel Chionh <gabrielchionh@gmail.com>
2024-09-07 12:44:24 +1200

    Added a comma to the choice message.

--- game.py
+++ game.py
@@ -5,8 +5,8 @@ while True:
     print(player_choice)
     computer_choice = random.randint(1,3)
     if computer_choice == 1:
-        print("You chose " + player_choice, "I chose rock")
+        print("You chose " + player_choice + ", I chose rock")
     if computer_choice == 2:
-        print("You chose " + player_choice, "I chose paper")
+        print("You chose " + player_choice + ", I chose paper")
     if computer_choice == 3:
-        print("You chose " + player_choice, "I chose scissors")
+        print("You chose " + player_choice + ", I chose scissors")
