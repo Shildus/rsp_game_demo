939078c79a2b9e6635cf937a0e1001b2d099b306
Mega-Roman <rshiltsov@gmail.com>
2024-09-07 12:34:48 +1200

    The computer will check your input to make sure it's valid

--- game.py
+++ game.py
@@ -2,4 +2,15 @@
 print("Welcome to our rock paper scissors game!")
 while True:
     player_choice = input("Please enter your choice from rock, paper or scissors. ")
-    print(player_choice)
+
+    if player_choice.lower() == "rock" or player_choice.lower() == "r":
+        print('Chosen rock')
+
+    elif player_choice.lower() == "paper" or player_choice.lower() == "p":
+        print('Chosen paper')
+
+    elif player_choice.lower() == "scissors" or player_choice.lower() == "s":
+        print('Chosen scissors')
+
+    else:
+        print('please choose a valid input')        
