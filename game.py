c6df35e63ee179056c4ae7e0dab008a9de1bf3de
RhysDaBoi <rhysww1@outlook.com>
2024-09-07 12:53:03 +1200

    I have added the thank you message and made it possible to be q instead of quit

--- game.py
+++ game.py
@@ -3,9 +3,10 @@ print("Welcome to our rock paper scissors game!")
 print("If you wish to quit type 'quit' into the console")
 while True:
     player_choice = input("Please enter your choice from rock, paper or scissors. ")
-    if player_choice.lower() == "quit":
-        break
-    print(player_choice)
+
+    if player_choice.lower() == "quit" or player_choice.lower() == "q":
+      print("Thank you so much for playing our game")
+      break
 
     computer_choice = random.randint(1,3)
     if computer_choice == 1:
@@ -14,4 +15,3 @@ while True:
         print("You chose " + player_choice + ", I chose paper")
     if computer_choice == 3:
         print("You chose " + player_choice + ", I chose scissors")
-
